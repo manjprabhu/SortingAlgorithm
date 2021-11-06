a676268738e82afcf0d88de385bd34d47b4ad45f
MPB <mp4learning@gmail.com>
2021-11-06 14:07:37 +0530

    Driver app changes

--- app/src/main/java/com/example/sortinalgorithm/MainActivity.java
+++ app/src/main/java/com/example/sortinalgorithm/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         quickSorting.StartSorting();
 
         SelectionSort selectionSort = new SelectionSort();
-        selectionSort.StartSelectionSort();
+//        selectionSort.StartSelectionSort();
+
+        BubbleSort bubbleSort = new BubbleSort();
     }
 }
